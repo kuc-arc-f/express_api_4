38c9424debcd8ef001b5b938f7c936aa6aa9b05c
中島浩二 <knaka0209@gmail.com>
2022-05-17 15:48:05 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+﻿# express_api_4
+
+ Version: 0.9.1
+
+ Author  : Kouji Nakashima / kuc-arc-f.com
+
+ date    : 2022/05/14 
+
+ update :  2022/05/17  
+
+***
+
+express + postgres , API sample
+
+* express : 4.17.1
+
+* typescript: 4.6.4
+
+* pg: 8.7.3
+
+***
+### setup
+
+* .env : password , host , dbname set
+
+```
+POSTGRES_DATABASE="__DBNAME__"
+POSTGRES_USER="__USER__"
+POSTGRES_PASSWORD="__PASSWORD__"
+POSTGRES_HOST="__DBHOST__"
+POSTGRES_PORT=5432
+```
+
+***
+### Start
+
+```
+# dev-start
+yarn dev
+
+# start
+yarn build
+
+yarn start
+
+```
+***
+### blog
+
+***
+
---
+++ src/lib/LibTask.ts
@@ -0,0 +1,67 @@
+import LibConfig from './LibConfig';
+require('dotenv').config();
+//const pg = require("pg");
+//const { Pool, Client } = require('pg')
+//const { Pool } = require('pg')
+import LibPg from './LibPg';
+
+const  LibTask = {
+  getItems :async function(){
+    try {
+      const text = `
+       SELECT * FROM public."Task" ORDER BY id DESC LIMIT 100
+      `;
+      const client = LibPg.getClient();
+      const res = await client.query(text);
+      client.end();
+      /*
+      const pool = LibPg.getPool();
+      const res = await pool.query(text)
+      await pool.end();
+      */      
+      console.log(res.rows);
+      return res.rows;      
+    } catch (err) {
+      console.error(err);
+      throw new Error('Error , getItems:' +err);
+    }          
+  },    
+  getTask :async function(id: number){
+  },
+  addTask :async function(req: any){
+    try {
+//console.log(LibConfig.OK_CODE);
+console.log(req.body);
+      const body = req.body;
+      const text = `
+      INSERT INTO public."Task" (title, content, "userId", "createdAt", "updatedAt") 
+      VALUES($1, $2, 0, current_timestamp, current_timestamp) RETURNING *
+      `;      
+      const values = [body.title, body.content ]
+      /*
+      const text = `
+      INSERT INTO public."Task" (title, content, "userId", "createdAt", "updatedAt") 
+      VALUES('${body.title}', '${body.content}', 0, current_timestamp, current_timestamp) RETURNING *
+      `;
+      */
+//console.log(text);
+      const client = LibPg.getClient();
+      const res = await client.query(text, values);
+      client.end();
+      const result = res.rows[0];
+console.log(result);
+      return {
+        ret: LibConfig.OK_CODE, data: result 
+      };
+//      return res.rows[0];
+    } catch (err) {
+      console.error(err);
+      throw new Error('Error , addTask: '+ err);
+    }    
+  },
+  updateTask :async function(args: any){
+  },  
+  deleteTask :async function(args: any){
+  },             
+}
+export default LibTask;
